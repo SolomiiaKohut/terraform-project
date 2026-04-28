ddd8cb95a88beb89ec237b2472070fd6b1f12160
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 09:58:09 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "do_token" {}
+variable "spaces_access_key" {}
+variable "spaces_secret_key" {}
+variable "ssh_key_id" {}
+
+variable "name" {
+  default = "kohut"
+}
+
+variable "region" {
+  default = "fra1"
+}
